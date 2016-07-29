4e03428d2f4440bfdeb21fb7ef2ab2dd33f907a6
Alex Mos <alexander.moskovkin@devexpress.com>
2016-07-29 14:37:42 +0300

    publish api classes and consts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import { readSync as read } from 'read-file-relative';
 import CompilerAdapter from './compiler';
+import TestRun from './test-run';
+import TEST_RUN_ERROR_TYPE from './test-run-error/type';
+import TestRunErrorFormattableAdapter from './test-run-error/formattable-adapter';
+
+const CLIENT_RUNNER_SCRIPT = read('./client/index.js');
 
 export default {
-    Compiler: CompilerAdapter
+    TEST_RUN_ERROR_TYPE:            TEST_RUN_ERROR_TYPE,
+    CLIENT_RUNNER_SCRIPT:           CLIENT_RUNNER_SCRIPT,
+    Compiler:                       CompilerAdapter,
+    TestRun:                        TestRun,
+    TestRunErrorFormattableAdapter: TestRunErrorFormattableAdapter
 };
